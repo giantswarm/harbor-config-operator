ff1d5aac227c21e6a6b662220ea1c320e5851e3e
William Young <82178997+William-Young-97@users.noreply.github.com>
2022-11-21 15:14:34 +0000

    Dev (#3)
    * Init go mod v1
    
    * Init kubebuilder
    
    * Crate api
    
    * Add registry logic
    
    * Make manifests
    
    * Add broken project logic
    
    * Add replication rule logic
    
    * Working replication logic
    
    * Add registry update logic
    
    * Add update + refactor
    
    * Control flow refactor
    
    * Added cr fields; replication execution not working
    
    * Update README.md
    
    * Fixed bugs; Replication runs on create
    
    * wip pulling harbor cluster cr
    
    * Get harborcluster resource complete
    
    * Refactor code in reconcile
    
    * Further refactoring of reconiliation loop
    
    * Username passed in through cr
    
    * Refactored replication trigger
    
    * Decoupled registry name from CRD
    
    * Add correct location for registryname
    
    * Get harbor client password via typed client
    
    Signed-off-by: William Young <will.young@engineerbetter.com>
    
    * Refactored config client setup
    Added RBAC permissions
    Updated the controller to communicate internally
    
    * Added external kube config option for local development
    
    Signed-off-by: Dipo Ajayi <ajayidipo@ymail.com>
    
    * Removed configuration-cr.yaml file to samples
    Removed replicationFound.ID field from ReplicationPolicy update struct
    
    Signed-off-by: Dipo Ajayi <ajayidipo@ymail.com>
    
    * Renamed API group
    
    harbor-configuration causes confusion because a similar name already exist from harbor operator
    
    Signed-off-by: Dipo Ajayi <ajayidipo@ymail.com>
    
    * Renamed image name to harbor-config-operator to make it more specific
    
    Signed-off-by: Dipo Ajayi <ajayidipo@ymail.com>
    
    * Added Go format dependencies to Makefile
    
    Signed-off-by: Dipo Ajayi <ajayidipo@ymail.com>
    
    * Refactored creating NewProject logic in reconciler
    
    Signed-off-by: Dipo Ajayi <ajayidipo@ymail.com>
    
    * Made repititive harborError calls into vars
    
    * Refactored projectReconciliation method for when storage limit is set
    
    * Refactored registryReconciliation method
    
    * Refactored replicationRuleReconciliation
    
    Signed-off-by: William Young <will.young@engineerbetter.com>
    Signed-off-by: Dipo Ajayi <ajayidipo@ymail.com>
    Co-authored-by: Oladipupo Ajayi <ajayidipo@ymail.com>

---
+++ api/v1alpha1/harborconfiguration_types.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func init() {
+	SchemeBuilder.Register(&HarborConfiguration{}, &HarborConfigurationList{})
+}
+
+type HarborConfigurationSpec struct {
+	HarborTarget HarborTarget `json:"harborTarget,omitempty"`
+	Registry     Registry     `json:"registry,omitempty"`
+	ProjectReq   ProjectReq   `json:"projectReq,omitempty"`
+	Replication  Replication  `json:"replication,omitempty"`
+}
+
+type HarborConfigurationStatus struct {
+	RegistryId    int64  `json:"registryId,omitempty"`
+	ProjectId     string `json:"projectId,omitempty"`
+	ReplicationId int64  `json:"replicationId,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+type HarborConfiguration struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HarborConfigurationSpec   `json:"spec,omitempty"`
+	Status HarborConfigurationStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+type HarborConfigurationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HarborConfiguration `json:"items,omitempty"`
+}
+
+type HarborTarget struct {
+	Name           string `json:"name,omitempty"`
+	Namespace      string `json:"namespace,omitempty"`
+	HarborUsername string `json:"harborUsername,omitempty"`
+}
+
+type Registry struct {
+	Name              string              `json:"name,omitempty"`
+	Type              string              `json:"type,omitempty"`
+	TargetRegistryUrl string              `json:"targetRegistryUrl,omitempty"`
+	Description       string              `json:"description,omitempty"`
+	Credential        *RegistryCredential `json:"credential,omitempty"`
+}
+
+type RegistryCredential struct {
+
+	// Access key, e.g. user name when credential type is 'basic'.
+	AccessKey string `json:"access_key,omitempty"`
+
+	// Access secret, e.g. password when credential type is 'basic'.
+	AccessSecret string `json:"access_secret,omitempty"`
+
+	// Credential type, such as 'basic', 'oauth'.
+	Type string `json:"type,omitempty"`
+}
+
+type ProjectReq struct {
+	ProjectName  string `json:"projectName,omitempty"`
+	StorageLimit *int64 `json:"storageLimit,omitempty"`
+	IsPublic     *bool  `json:"isPublic,omitempty"`
+}
+
+type Replication struct {
+	Name                 string               `json:"name,omitempty"`
+	RegistryName         string               `json:"registryName,omitempty"`
+	DestinationNamespace string               `json:"destinationNamespace,omitempty"`
+	Description          string               `json:"description,omitempty"`
+	DestinationRegistry  *apiextensions.JSON  `json:"destinationRegistry,omitempty"`
+	EnablePolicy         bool                 `json:"enablePolicy,omitempty"`
+	ReplicateDeletion    bool                 `json:"replicateDeletion,omitempty"`
+	Override             bool                 `json:"override,omitempty"`
+	Filters              []apiextensions.JSON `json:"filters,omitempty"`
+	Trigger              *apiextensions.JSON  `json:"trigger,omitempty"`
+}
---
+++ controllers/harborconfiguration_controller.go
@@ -0,0 +1,424 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	harborOperator "github.com/goharbor/harbor-operator/apis/goharbor.io/v1beta1"
+	"github.com/goharbor/harbor-operator/pkg/cluster/k8s"
+	rep "github.com/mittwald/goharbor-client/v5/apiv1/replication"
+	apiv2 "github.com/mittwald/goharbor-client/v5/apiv2"
+	modelv2 "github.com/mittwald/goharbor-client/v5/apiv2/model"
+	harborerrors "github.com/mittwald/goharbor-client/v5/apiv2/pkg/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	harborconfigurationv1alpha1 "github.com/giantswarm/harbor-config-operator/api/v1alpha1"
+)
+
+var (
+	harborClusterGVM = schema.GroupVersionResource{
+		Group:    "goharbor.io",
+		Version:  "v1alpha3",
+		Resource: "harborclusters",
+	}
+)
+
+// HarborConfigurationReconciler reconciles a HarborConfiguration object
+type HarborConfigurationReconciler struct {
+	DClient *k8s.DynamicClientWrapper
+	client.Client
+	*runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=administration.harbor.configuration,resources=harborconfigurations,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=administration.harbor.configuration,resources=harborconfigurations/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=administration.harbor.configuration,resources=harborconfigurations/finalizers,verbs=update
+//+kubebuilder:rbac:groups=goharbor.io,resources=harborclusters,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=goharbor.io,resources=harborclusters/status,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=goharbor.io,resources=harborclusters/finalizers,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=secrets;services,verbs=get;list
+
+func (r *HarborConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	var harborConfiguration harborconfigurationv1alpha1.HarborConfiguration
+	err := r.Get(ctx, req.NamespacedName, &harborConfiguration)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	dynamicClient, err := getDynamicKubeConfig()
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	crdClient := dynamicClient.Resource(harborClusterGVM).Namespace(harborConfiguration.Spec.HarborTarget.Namespace)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	var harborTarget harborOperator.HarborCluster
+	harborTarget, err = getConcreteHarborType(ctx, crdClient, harborConfiguration, harborTarget)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	clientSet, err := getTypedKubeConfig()
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	haborSecret, err := getHarborSecret(ctx, clientSet, &harborTarget)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	client, err := apiv2.NewRESTClientForHost(getHarborURL(&harborTarget), harborConfiguration.Spec.HarborTarget.HarborUsername, haborSecret, nil)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if harborConfiguration.ObjectMeta.DeletionTimestamp.IsZero() {
+		_, err = r.reconcileAll(ctx, harborConfiguration, client)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		_, err = triggerReplication(ctx, harborConfiguration, client)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	} else {
+		_, err = deleteAll(ctx, harborConfiguration, client)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *HarborConfigurationReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&harborconfigurationv1alpha1.HarborConfiguration{}).
+		Complete(r)
+}
+
+func (r *HarborConfigurationReconciler) registryReconciliation(ctx context.Context, harborConfiguration harborconfigurationv1alpha1.HarborConfiguration, registry modelv2.Registry, client *apiv2.RESTClient) (ctrl.Result, error) {
+	err := client.NewRegistry(ctx, &registry)
+	if errors.Is(err, &harborerrors.ErrRegistryNameAlreadyExists{}) {
+		update := &modelv2.RegistryUpdate{
+			Name:        &harborConfiguration.Spec.Registry.Name,
+			URL:         &harborConfiguration.Spec.Registry.TargetRegistryUrl,
+			Description: &harborConfiguration.Spec.Registry.Description,
+		}
+		if harborConfiguration.Spec.Registry.Credential != nil {
+			update.AccessKey = &harborConfiguration.Spec.Registry.Credential.AccessKey
+			update.AccessSecret = &harborConfiguration.Spec.Registry.Credential.AccessSecret
+			update.CredentialType = &harborConfiguration.Spec.Registry.Credential.Type
+		}
+		srcRegistry, err := client.GetRegistryByName(ctx, harborConfiguration.Spec.Replication.RegistryName)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		err = client.UpdateRegistry(ctx, update, srcRegistry.ID)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	} else {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *HarborConfigurationReconciler) projectReconciliation(ctx context.Context, harborConfiguration harborconfigurationv1alpha1.HarborConfiguration, registry modelv2.Registry, client *apiv2.RESTClient) (ctrl.Result, error) {
+	srcRegistry, err := client.GetRegistryByName(ctx, harborConfiguration.Spec.Replication.RegistryName)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	requestedProject := &modelv2.ProjectReq{
+		ProjectName:  harborConfiguration.Spec.ProjectReq.ProjectName,
+		Public:       harborConfiguration.Spec.ProjectReq.IsPublic,
+		StorageLimit: harborConfiguration.Spec.ProjectReq.StorageLimit,
+		RegistryID:   &srcRegistry.ID,
+	}
+
+	err = client.NewProject(ctx, requestedProject)
+	if errors.Is(err, &harborerrors.ErrProjectNameAlreadyExists{}) {
+		existingProject, err := client.GetProject(ctx, requestedProject.ProjectName)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		updatedProject := &modelv2.Project{
+			Name:       harborConfiguration.Spec.ProjectReq.ProjectName,
+			RegistryID: srcRegistry.ID,
+			ProjectID:  existingProject.ProjectID,
+		}
+		// Note: Only positive values of storageLimit are supported through this method.
+		// Use the 'UpdateStorageQuotaByProjectID' method when `project.StorageLimit`is `-1`
+		unlimitedStorage := int64(-1)
+		if requestedProject.StorageLimit != &unlimitedStorage {
+			err = client.UpdateProject(ctx, updatedProject, requestedProject.StorageLimit)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		} else {
+			err = client.UpdateStorageQuotaByProjectID(ctx, int64(updatedProject.ProjectID), unlimitedStorage)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *HarborConfigurationReconciler) replicationRuleReconciliation(ctx context.Context, harborConfiguration harborconfigurationv1alpha1.HarborConfiguration, registry modelv2.Registry, client *apiv2.RESTClient) (ctrl.Result, error) {
+	srcRegistry, err := client.GetRegistryByName(ctx, harborConfiguration.Spec.Replication.RegistryName)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	reqFilters := make([]*modelv2.ReplicationFilter, 0)
+	for _, v := range harborConfiguration.Spec.Replication.Filters {
+		temp := modelv2.ReplicationFilter{}
+		err := json.Unmarshal(v.Raw, &temp)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		reqFilters = append(reqFilters, &temp)
+	}
+
+	var reqDestinationRegistry *modelv2.Registry
+	if harborConfiguration.Spec.Replication.DestinationRegistry != nil {
+		err = json.Unmarshal(harborConfiguration.Spec.Replication.DestinationRegistry.Raw, reqDestinationRegistry)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+	}
+
+	var reqTrigger *modelv2.ReplicationTrigger
+	if harborConfiguration.Spec.Replication.Trigger != nil {
+		err = json.Unmarshal(harborConfiguration.Spec.Replication.Trigger.Raw, &reqTrigger)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	err = client.NewReplicationPolicy(ctx,
+		reqDestinationRegistry,
+		srcRegistry,
+		harborConfiguration.Spec.Replication.ReplicateDeletion,
+		harborConfiguration.Spec.Replication.Override,
+		harborConfiguration.Spec.Replication.EnablePolicy,
+		reqFilters,
+		reqTrigger,
+		harborConfiguration.Spec.Replication.DestinationNamespace,
+		harborConfiguration.Spec.Replication.Description,
+		harborConfiguration.Spec.Replication.Name)
+
+	if errors.Is(err, &rep.ErrReplicationNameAlreadyExists{}) {
+		update := modelv2.ReplicationPolicy{
+			Name:          harborConfiguration.Spec.Replication.Name,
+			Description:   harborConfiguration.Spec.Replication.Description,
+			SrcRegistry:   srcRegistry,
+			DestNamespace: harborConfiguration.Spec.Replication.DestinationNamespace,
+			DestRegistry:  reqDestinationRegistry,
+			Filters:       reqFilters,
+			Trigger:       reqTrigger,
+			Override:      harborConfiguration.Spec.Replication.Override,
+			Enabled:       harborConfiguration.Spec.Replication.EnablePolicy,
+		}
+
+		replicationFound, err := client.GetReplicationPolicyByName(ctx, harborConfiguration.Spec.Replication.Name)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		err = client.UpdateReplicationPolicy(ctx, &update, replicationFound.ID)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	} else {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *HarborConfigurationReconciler) reconcileAll(ctx context.Context, harborConfiguration harborconfigurationv1alpha1.HarborConfiguration, client *apiv2.RESTClient) (ctrl.Result, error) {
+	registry := &modelv2.Registry{
+		Name:        harborConfiguration.Spec.Registry.Name,
+		Type:        harborConfiguration.Spec.Registry.Type,
+		URL:         harborConfiguration.Spec.Registry.TargetRegistryUrl,
+		Description: harborConfiguration.Spec.Registry.Description,
+		Credential:  (*modelv2.RegistryCredential)(harborConfiguration.Spec.Registry.Credential),
+	}
+
+	_, err := r.registryReconciliation(ctx, harborConfiguration, *registry, client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	_, err = r.projectReconciliation(ctx, harborConfiguration, *registry, client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	_, err = r.replicationRuleReconciliation(ctx, harborConfiguration, *registry, client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, err
+}
+
+func getHarborSecret(ctx context.Context, clientSet *kubernetes.Clientset, harborcluster *harborOperator.HarborCluster) (string, error) {
+	passwordSecret, err := clientSet.CoreV1().Secrets(harborcluster.Namespace).Get(ctx, harborcluster.Spec.HarborAdminPasswordRef, v1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	return string(passwordSecret.Data["secret"]), nil
+}
+
+func getHarborURL(harborcluster *harborOperator.HarborCluster) string {
+	url := fmt.Sprintf("http://%s-harbor-harbor-core.%s/api/v2.0", harborcluster.Name, harborcluster.Namespace)
+	return url
+}
+
+func getTypedKubeConfig() (*kubernetes.Clientset, error) {
+	var config *rest.Config
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		config, err = getkubeConfig()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	kubeConfig, err := kubernetes.NewForConfig(config)
+
+	return kubeConfig, err
+}
+
+func getDynamicKubeConfig() (dynamic.Interface, error) {
+	var config *rest.Config
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		config, err = getkubeConfig()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("error creating dynamic client: %v\n", err)
+		os.Exit(1)
+	}
+
+	return dynamicClient, err
+}
+
+func getkubeConfig() (*rest.Config, error) {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("error getting user home dir: %v\n", err)
+		os.Exit(1)
+	}
+	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+	fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		fmt.Printf("error getting Kubernetes config: %v\n", err)
+		os.Exit(1)
+	}
+
+	return kubeConfig, err
+}
+
+func getConcreteHarborType(ctx context.Context, crdClient dynamic.ResourceInterface, harborConfiguration harborconfigurationv1alpha1.HarborConfiguration, harborTarget harborOperator.HarborCluster) (harborOperator.HarborCluster, error) {
+	harborUnstructured, err := crdClient.Get(ctx, harborConfiguration.Spec.HarborTarget.Name, v1.GetOptions{
+		TypeMeta: v1.TypeMeta{
+			Kind:       "HarborCluster",
+			APIVersion: "v1alpha3",
+		},
+	})
+	if err != nil {
+		return harborTarget, err
+	}
+
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(harborUnstructured.UnstructuredContent(), &harborTarget)
+	if err != nil {
+		return harborTarget, err
+	}
+	return harborTarget, err
+}
+
+func deleteAll(ctx context.Context, harborConfiguration harborconfigurationv1alpha1.HarborConfiguration, client *apiv2.RESTClient) (ctrl.Result, error) {
+	err := client.DeleteRegistryByID(ctx, harborConfiguration.Status.RegistryId)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = client.DeleteProject(ctx, harborConfiguration.Status.ProjectId)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = client.DeleteReplicationPolicyByID(ctx, harborConfiguration.Status.ReplicationId)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+func triggerReplication(ctx context.Context, harborConfiguration harborconfigurationv1alpha1.HarborConfiguration, client *apiv2.RESTClient) (ctrl.Result, error) {
+	replicationFound, err := client.GetReplicationPolicyByName(ctx, harborConfiguration.Spec.Replication.Name)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	trigger := &modelv2.StartReplicationExecution{
+		PolicyID: replicationFound.ID,
+	}
+	err = client.TriggerReplicationExecution(ctx, trigger)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, err
+}
