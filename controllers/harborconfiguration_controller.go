6f5ee22e3d70b77bcd932641ce01d12faf45844b
William Young <will.young@engineerbetter.com>
2023-01-18 16:08:36 +0000

    Decouple registry from replication; Add proxy cache project example

--- controllers/harborconfiguration_controller.go
+++ controllers/harborconfiguration_controller.go
@@ -153,7 +153,7 @@ func (r *HarborConfigurationReconciler) registryReconciliation(ctx context.Conte
 			update.AccessSecret = &harborConfiguration.Spec.Registry.Credential.AccessSecret
 			update.CredentialType = &harborConfiguration.Spec.Registry.Credential.Type
 		}
-		srcRegistry, err := client.GetRegistryByName(ctx, harborConfiguration.Spec.Replication.RegistryName)
+		srcRegistry, err := client.GetRegistryByName(ctx, harborConfiguration.Spec.Registry.Name)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
